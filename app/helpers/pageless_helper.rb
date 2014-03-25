945d466640823f146c8192bafd6c9ed16e7ab228
ratnakar <ratnakarrao_nyros@yahoo.com>
2014-03-25 15:01:04 +0530

    helper class added

--- app/helpers/pageless_helper.rb
+++ app/helpers/pageless_helper.rb
@@ -1,12 +1,16 @@
-def pageless(total_pages, url=nil, container=nil)
-  opts = {
-      :totalPages => total_pages,
-      :url        => url,
-      :loaderMsg  => 'Loading more pages...',
-      :loaderImage => image_path('load.gif')
-  }
+module PagelessHelper 
 
-  container && opts[:container] ||= container
+	def pageless(total_pages, url=nil, container=nil)
+	  opts = {
+	      :totalPages => total_pages,
+	      :url        => url,
+	      :loaderMsg  => 'Loading more pages...',
+	      :loaderImage => image_path('load.gif')
+	  }
 
-  javascript_tag("$('#results').pageless(#{opts.to_json});")
+	  container && opts[:container] ||= container
+
+	  javascript_tag("$('#results').pageless(#{opts.to_json});")
+	end
+
 end
